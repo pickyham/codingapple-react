1e89e66bbd087c952df748059dd0a85da1855c99
Hyerim Yeom <ringkle777@gmail.com>
2021-01-19 01:07:35 +0900

    #1 part1 : state
    - ES6 destructuring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  let [글제목,글제목변경함수] = useState('남자 코트 추천');
+  //[state데이터, state데이터변경함수]
+  //state는 1. 변수 대신 쓰는 데이터 저장공간 2. useState()를 이용해 만들어야함
   let post ='강남 불고기 맛집'
-  let title ='제목'
-  let content ='내용'
 
   return (
     <div className="App">
@@ -15,12 +17,8 @@ function App() {
       </div>
       
       </div>
-      {/* <div className="list">
-          <ol>{title}</ol>
-          <ul>{content}</ul>
-      </div> */}
       <div className="list">
-        <h3>{ post }</h3>
+        <h3>{ 글제목 }</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
