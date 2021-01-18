3f7a4f2e1ccb173fee2ee47398c5affd7c164c1e
Hyerim Yeom <ringkle777@gmail.com>
2021-01-19 00:58:49 +0900

    #1 part1 : state
    - Make a list

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post ='강남 불고기 맛집'
+  let title ='제목'
+  let content ='내용'
+
   return (
     <div className="App">
       <div className="black-nav"> 
-      
-
       <div>
           개발 Blog 
       </div>
       
-
+      </div>
+      {/* <div className="list">
+          <ol>{title}</ol>
+          <ul>{content}</ul>
+      </div> */}
+      <div className="list">
+        <h3>{ post }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
       </div>
     </div>
   );
